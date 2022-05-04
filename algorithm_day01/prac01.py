74669f7522e955b31b28265df1b2a2bdc9cfe844
Chanhyun Wang <rubberramen@naver.com>
2022-05-05 00:26:43 +0900

    first algorithm practice

---
+++ algorithm_day01/prac01.py
@@ -0,0 +1,11 @@
+# 수열01 : 1+2+3+4+ ... +100까지의 합계
+
+num = 0
+answer = 0
+
+for i in range(100):
+    num += 1
+    answer += num
+
+print('\n문제 : 1+2+3+4+ ... +100 까지의 합계 구하기')
+print(f'정답 : {answer}')
---
+++ algorithm_day01/prac02.py
@@ -0,0 +1,14 @@
+# 수열01-2 : 1+3+5+7+...+99 까지의 합계
+
+num = 0
+answer = 0
+
+for i in range(100):
+    num += 1
+    if num % 2 != 0:
+        answer += num
+    else:
+        pass
+
+print('\n문제 : 1+3+5+7+...+99 까지의 합계 구하기')
+print(f'정답 : {answer}')
